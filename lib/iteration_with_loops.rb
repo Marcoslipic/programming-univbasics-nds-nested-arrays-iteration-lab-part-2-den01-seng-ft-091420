77055cf4a6f3a718bf4e2321d070a2f3f716530b
Marcos Lipic <mjlipic.37@gmail.com>
2020-08-25 23:32:14 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,5 +3,6 @@ def find_min_in_nested_arrays(src)
   # Produce a new Array that contains the smallest number of each of the nested arrays
   
   lowest_daily_temp = []
-  
+  row_index = 0 
+  while row_index < src.count
 end
